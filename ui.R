6b9a1a3272718e6998253d2c67a8f4010f862daf
E. Valencia <valenzia10@gmail.com>
2015-01-19 19:31:01 +0100

    Added support for negative polynomial coefficients.

--- ui.R
+++ ui.R
@@ -7,12 +7,12 @@ shinyUI(pageWithSidebar(
                tags$script( "MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});", type='text/x-mathjax-config')
     ),
     h4('Input the polynomial coefficients below'),
-    numericInput('id5','$\\alpha_5 \\cdot x^5$',0,min=0,max=20,step=0.1),
-    numericInput('id4','$\\alpha_4 \\cdot x^4$',0,min=0,max=20,step=0.1),
-    numericInput('id3','$\\alpha_3 \\cdot x^3$',0,min=0,max=20,step=0.1),
-    numericInput('id2','$\\alpha_2 \\cdot x^2$',0,min=0,max=20,step=0.1),
-    numericInput('id1','$\\alpha_1 \\cdot x^1$',0,min=0,max=20,step=0.1),
-    numericInput('id0','$\\alpha_0$',0,min=0,max=200,step=0.1),
+    numericInput('id5','$\\alpha_5 \\cdot x^5$',0,min=-20,max=20,step=0.1),
+    numericInput('id4','$\\alpha_4 \\cdot x^4$',0,min=-20,max=20,step=0.1),
+    numericInput('id3','$\\alpha_3 \\cdot x^3$',0,min=-20,max=20,step=0.1),
+    numericInput('id2','$\\alpha_2 \\cdot x^2$',0,min=-20,max=20,step=0.1),
+    numericInput('id1','$\\alpha_1 \\cdot x^1$',0,min=-20,max=20,step=0.1),
+    numericInput('id0','$\\alpha_0$',0,min=-200,max=200,step=0.1),
     submitButton('Plot')
   ),
   mainPanel(
